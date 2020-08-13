7e9e7a04cece6c7a166b318011e780a663b354bc
caoguobin <caoguobin@live.com>
2020-08-13 18:51:35 +0800

    添加收益记录和提现记录页面

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,23 @@
 <template>
   <div style="background: #F8F8F8">
     首页
+    <p>{{ $route.query.id }}</p>
   </div>
 </template>
 
 <script>
 export default {
-name: "Home"
+  name: "Home",
+  mounted() {
+    let appId="wxc77be68b0f679a69";
+    let state = 'mytest';
+    let redirectUrl='http://wx.rainbase.cn/wx/setcode';
+    redirectUrl=encodeURI(redirectUrl);
+    let h = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxc77be68b0f679a69&redirect_uri='+redirectUrl+'&response_type=code&scope=snsapi_userinfo&state='+state+'#wechat_redirect';
+    alert(h)
+    console.log(h)
+    window.location.href=h;
+  }
 }
 </script>
 
